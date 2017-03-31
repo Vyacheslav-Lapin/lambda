a0c70139fd2b1829d8b78700a79bcde0adc3703b
Egor Kozyrev <egor_kozyrev@epam.com>
2017-03-31 15:22:32 +0300

    exercise 1

--- src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
@@ -1,13 +1,19 @@
 package lambda.part1.exercise;
 
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import data.Person;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class Lambdas01Exercise {
 
@@ -19,13 +25,23 @@ class Lambdas01Exercise {
                 new Person("name 2", "lastName 1", 30)
         };
 
-        // TODO use Arrays.sort
+        Arrays.sort(persons, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                if (o1.getAge() > o2.getAge())
+                    return 1;
+                else if (o1.getAge() < o2.getAge())
+                    return -1;
+                else
+                    return 0;
+            }
+        });
 
-        assertArrayEquals(persons, new Person[]{
+        assertThat(persons, is(new Person[]{
                 new Person("name 3", "lastName 3", 20),
                 new Person("name 2", "lastName 1", 30),
                 new Person("name 1", "lastName 2", 40),
-        });
+        }));
     }
 
     @Test
@@ -36,10 +52,17 @@ class Lambdas01Exercise {
                 new Person("name 2", "lastName 1", 30)
         );
 
-        Person person = null;
+        final Optional<Person> personOptional =
+                FluentIterable.from(persons)
+                        .firstMatch(new Predicate<Person>() {
+                            public boolean apply(Person p) {
+                                return p.getAge() == 30;
+                            }
+                        });
 
-        // TODO use FluentIterable
-
-        assertEquals(person, new Person("name 1", "lastName 2", 30));
+        if (personOptional.isPresent())
+            assertThat(personOptional.get(), is(new Person("name 1", "lastName 2", 30)));
+        else
+            fail("");
     }
 }
