a15c30e23fac15a116659d83a27a0e103239c701
Ivan Fedorov <Ivan_Fedorov@epam.com>
2017-04-01 12:55:57 +0300

    homework 2

--- src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
@@ -1,9 +1,12 @@
 package lambda.part1.exercise;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import data.Person;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -18,7 +21,9 @@ class Lambdas02Exercise {
                 new Person("name 2", "lastName 1", 30)
         };
 
-        // TODO use Arrays.sort
+        Arrays.sort(persons, (o1, o2) -> o1.getFirstName().compareTo(o2.getFirstName()) * (-1));
+
+
 
         assertArrayEquals(persons, new Person[]{
                 new Person("name 3", "lastName 3", 20),
@@ -37,7 +42,11 @@ class Lambdas02Exercise {
 
         Person person = null;
 
-        // TODO use FluentIterable
+        final Optional<Person> personOptional =
+                FluentIterable.from(persons)
+                        .firstMatch(p -> p.getFirstName().equals("name 1"));
+
+        if (personOptional.isPresent()) person = personOptional.get();
 
         assertEquals(person, new Person("name 1", "lastName 2", 30));
     }
