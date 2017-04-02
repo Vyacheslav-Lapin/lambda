b6acd80038c70fbace1916289390c37a93691df2
wopqw <wopqw@yandex.ru>
2017-04-03 02:08:51 +0300

    lambdas01Exercise

--- src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
@@ -4,10 +4,12 @@ import com.google.common.collect.ImmutableList;
 import data.Person;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 class Lambdas01Exercise {
 
@@ -21,11 +23,24 @@ class Lambdas01Exercise {
 
         // TODO use Arrays.sort
 
-        assertArrayEquals(persons, new Person[]{
+//        Java 7
+//        Arrays.sort(persons, new Comparator<Person>() {
+//            @Override
+//            public int compare(Person o1, Person o2) {
+//                return o1.getAge() - o2.getAge();
+//            }
+//        });
+
+//        Java 8
+        persons = Stream.of(persons)
+                .sorted(Comparator.comparing(Person::getAge))
+                .toArray(Person[]::new);
+
+        assertThat(persons, is(new Person[]{
                 new Person("name 3", "lastName 3", 20),
                 new Person("name 2", "lastName 1", 30),
                 new Person("name 1", "lastName 2", 40),
-        });
+        }));
     }
 
     @Test
@@ -40,6 +55,26 @@ class Lambdas01Exercise {
 
         // TODO use FluentIterable
 
-        assertEquals(person, new Person("name 1", "lastName 2", 30));
+//        Java 7
+//        final Optional<Person> personOptional =
+//                FluentIterable.from(persons)
+//                .firstMatch(new Predicate<Person>() {
+//                    @Override
+//                    public boolean apply(Person p) {
+//                        return p.getAge() == 30;
+//                    }
+//                });
+//
+//        if(personOptional.isPresent())
+//            person = personOptional.get();
+
+//        Java 8
+        person = persons.stream()
+                .filter(p -> p.getAge() ==30)
+                .findFirst()
+                .orElse(new Person("n","ln", 99999));
+
+        assertThat(person, is(new Person("name 1", "lastName 2", 30)));
     }
 }
+
